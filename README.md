987270dfa78c32f187b5036fcc44b27b17c004c5
DEADushka108 <cloxx108@gmail.com>
2021-05-19 16:01:20 +0500

    Finish

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# informservice
+# Informservice test.
+
+### Author: [Kuznetsov Andrey](https://github.com/DEADushka108)
+
+### Mail: <aakuznetsov.mgn@gmail.com>
+
+### [Firebase version](https://orgchart-ca62f.web.app/)
+
+All you need to do is fork this repository to you PC and run follow command:
+
+---
+## Command
+
+* npm i - install all dependencies
+* npm run dev - development mode
+* npm run build - production mode
+* npm start - webpack-dev-server [link-to](http://localhost:2020/)
+* npm test - stylelint, eslint
+* npm run stylelint - check .scss files(config from HTMLAcademy)
+* npm run eslint - check .js and .jsx files(config from HTMLAcademy)
+
+---
+
+Enjoy :)
--- src/js/components/main/main.jsx
+++ src/js/components/main/main.jsx
@@ -15,13 +15,13 @@ const Main = (props) => {
       <div className="page-main__wrapper">
         <h1 className="visually-hidden">Main page</h1>
         <section className="task">
-          <div className="task__orgchart">
-            <img src="./img/content/orgchart.jpg" alt="Task1 image" />
+          <div className="task__wrapper">
+            <img className="task__image" width="300" height="169" src="./img/content/orgchart.jpg" alt="Task1 image" />
             <ul className="task__list">
               <li className="task__item">
                 <p className="task__text">
                   <span className="task__type">Task: </span>
-                  <span className="task__value">create orgchart</span>
+                  <span className="task__value">Create orgchart</span>
                 </p>
               </li>
               <li className="task__item">
@@ -33,17 +33,24 @@ const Main = (props) => {
               <li className="task__item">
                 <p className="task__text">
                   <span className="task__type">Used technology: </span>
-                  <span className="task__value">React, Redux, Webpack</span>
+                  <span className="task__value">React, Redux, Webpack, Sass</span>
                 </p>
               </li>
             </ul>
             <div className="task__links">
-              <a className="task__link" href="#">How to use</a>
+              <a
+                className="task__link"
+                href="https://docs.google.com/document/d/1xYvYwHXfpwSTjAdrtSOXkKl3qwKiYdpKSXIcni_67KY/edit?usp=sharing"
+                rel="noreferrer"
+                target="_blank"
+              >
+                How to use
+              </a>
               <Link to={AppRoute.ORGCHART} className="task__link">Orgchart</Link>
             </div>
           </div>
-          <div className="task__orgchart">
-            <img src="./img/content/routing.jpg" alt="Task2 image" />
+          <div className="task__wrapper">
+            <img img className="task__image task__image--routing" width="300" height="176" src="./img/content/routing.jpg" alt="Task2 image" />
             <ul className="task__list">
               <li className="task__item">
                 <p className="task__text">
@@ -60,13 +67,20 @@ const Main = (props) => {
               <li className="task__item">
                 <p className="task__text">
                   <span className="task__type">Used technology: </span>
-                  <span className="task__value">React, Redux, Webpack</span>
+                  <span className="task__value">React, Redux, Webpack, Sass</span>
                 </p>
               </li>
             </ul>
             <div className="task__links">
-              <a className="task__link" href="#">How to use</a>
-              <Link to={AppRoute.ROUTING} className="task__link">Orgchart</Link>
+              <a
+                className="task__link"
+                href="https://docs.google.com/document/d/1wiorAFiElnysfZSYfg2E9IKYrX-F1vMOMDAUrwZfFA0/edit?usp=sharing"
+                rel="noreferrer"
+                target="_blank"
+              >
+                  How to use
+              </a>
+              <Link to={AppRoute.ROUTING} className="task__link">Routing</Link>
             </div>
           </div>
         </section>
